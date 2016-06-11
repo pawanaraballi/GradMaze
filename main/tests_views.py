6d965dc845ec2cf3a84daf3ae1916526e16b8b64
Johnny Knighten <jknigh28@gmail.com>
2016-06-11 15:43:32 -0400

    Added Unittests for Views

--- main/tests_views.py
+++ main/tests_views.py
@@ -33,3 +33,15 @@ class LogoutViewTestCase(TestCase):
         response = self.client.post('/GradMaze/accounts/logout', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'logged_out.html')
+
+
+class RegisterViewTestCase(TestCase):
+    def test_load_view(self):
+        """Test GET/POST of Login Page"""
+        response = self.client.get('/GradMaze/accounts/register', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
+
+        response = self.client.post('/GradMaze/accounts/register', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')

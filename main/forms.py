dcf835a5f45640a03e3255774b7c887dc9260f2b
Johnny Knighten <jknigh28@gmail.com>
2016-06-11 11:26:37 -0400

    Added Background color for contrast Form now catches empty fields

--- main/forms.py
+++ main/forms.py
@@ -11,6 +11,16 @@ class LoginForm(forms.Form):
     # Validation Takes Place Here
     def clean(self):
 
+
+        # Empty Username
+        if not self.cleaned_data.get('user_name', False):
+            raise ValidationError("Empty Username")
+
+        # Empty Password
+        if not self.cleaned_data.get('password', False):
+            raise ValidationError("Empty Password")
+
+
         # Try to authenticate user
         user = authenticate(username=self.cleaned_data['user_name'], password=self.cleaned_data['password'])
 
